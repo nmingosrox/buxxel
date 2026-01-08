f5902c1d3a7b1e1c3c5bc4410d94caf473181271
nmingosrox <70023818+nmingosrox@users.noreply.github.com>
2026-01-08 15:14:34 +0200

    Remove user_loader function from models.py
    Removed user_loader function for loading users by ID.

--- buxxel/models.py
+++ buxxel/models.py
@@ -3,11 +3,6 @@ from buxxel.extensions import db, login_manager
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-
 # --------------------
 # User Model
 # --------------------
