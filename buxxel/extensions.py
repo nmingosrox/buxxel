6f35ddc3f3251274f85ee5d3b71cc32cfc7b0277
nmingosrox <70023818+nmingosrox@users.noreply.github.com>
2026-01-08 15:49:23 +0200

    Remove user loader function from extensions.py
    Removed user loader function for Flask-Login.

--- buxxel/extensions.py
+++ buxxel/extensions.py
@@ -3,14 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_admin import Admin
-from buxxel.models import User
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 migrate = Migrate()
 admin = Admin()
-
-# Required: tell Flask-Login how to load a user from ID
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
