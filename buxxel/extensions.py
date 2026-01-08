cff4e0ab9278e398a8e7f367ab14c5a9e877a07a
nmingosrox <70023818+nmingosrox@users.noreply.github.com>
2026-01-08 15:48:12 +0200

    Import User model in extensions.py

--- buxxel/extensions.py
+++ buxxel/extensions.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_admin import Admin
+from buxxel.models import User
 
 db = SQLAlchemy()
 login_manager = LoginManager()
