04d5710cdb33d7ff2ad49bf82f39a817006d7815
nmingosrox <70023818+nmingosrox@users.noreply.github.com>
2026-01-11 20:38:31 +0200

    Update extensions.py

--- buxxel/extensions.py
+++ buxxel/extensions.py
@@ -1,5 +1,1 @@
-from supabase import create_client, Client
 
-# Initialize the client as None. It will be configured in the app factory.
-supabase: Client = None        # Standard client for user-specific operations
-supabase_admin: Client = None  # Privileged client for admin tasks
