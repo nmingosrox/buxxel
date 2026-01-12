6e815f090c599ac5384da47ce3fde034b67609d1
nmingosrox <70023818+nmingosrox@users.noreply.github.com>
2026-01-12 13:37:23 +0200

    Create password_reset.js

---
+++ buxxel/static/js/auth/password_reset.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('#forgot-password-link').on('click', ...);
+    $('#back-to-login-link').on('click', ...);
+    $('#resetPasswordRequestForm').on('submit', async function(e) {
+        // reset logic
+    });
+});
