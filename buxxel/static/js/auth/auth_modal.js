a12456f2e4a8bd733d8f05dfa61de9fa6227bbda
nmingosrox <70023818+nmingosrox@users.noreply.github.com>
2026-01-12 13:42:11 +0200

    Update auth_modal.js

--- buxxel/static/js/auth/auth_modal.js
+++ buxxel/static/js/auth/auth_modal.js
@@ -1,1 +1,22 @@
+$(document).ready(function() {
+    // --- RESET MODAL STATE ---
+    $('#authModal').on('hidden.bs.modal', function () {
+        // Hide and clear contextual message
+        $('#auth-context-message').hide().text('');
 
+        // Reset password reset view
+        $('#password-reset-view').hide();
+
+        // Show login/signup views again
+        $('#loginForm, #signup-view, hr.my-4').show();
+
+        // Reset modal title
+        $('#authModalLabel').text('Login or Create an Account');
+
+        // Clear any reset request alerts
+        $('#reset-request-alert').hide().text('');
+
+        // Reset all forms inside the modal
+        $('#signupForm, #loginForm, #resetPasswordRequestForm').trigger('reset');
+    });
+});
