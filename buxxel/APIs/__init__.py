96c02bee8e953ecaae75a067a838c16a29c0df2c
nmingosrox <70023818+nmingosrox@users.noreply.github.com>
2026-01-16 11:38:37 +0200

    Update __init__.py

--- buxxel/APIs/__init__.py
+++ buxxel/APIs/__init__.py
@@ -14,15 +14,15 @@ from .profiles_api import profiles_api_bp
 # Admin APIs
 from .admin.admin_listings_api import admin_listings_api_bp
 from .admin.admin_orders_api import admin_orders_api_bp
-from .admin.admin_users_api import admin_users_api_bp
+from .admin.admin_profiles_api import admin_users_api_bp
 
 __all__ = [
     # Core
     "listings_api_bp",
     "orders_api_bp",
-    "users_api_bp",
+    "profiles_api_bp",
     # Admin
     "admin_listings_api_bp",
     "admin_orders_api_bp",
-    "admin_users_api_bp",
+    "admin_profiles_api_bp",
 ]
