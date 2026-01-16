1975dff828829535ef3b972ba5a6b85ff5140c38
nmingosrox <70023818+nmingosrox@users.noreply.github.com>
2026-01-16 11:43:41 +0200

    Update admin_profiles_api.py

--- buxxel/APIs/admin/admin_profiles_api.py
+++ buxxel/APIs/admin/admin_profiles_api.py
@@ -2,9 +2,9 @@ from flask import Blueprint, jsonify, current_app
 from buxxel.decorators import admin_api_required
 from buxxel.database import supabase_admin
 
-admin_users_api_bp = Blueprint("admin_users_api", __name__, url_prefix="/api/admin/users")
+admin_profiles_api_bp = Blueprint("admin_users_api", __name__, url_prefix="/api/admin/users")
 
-@admin_users_api_bp.route("/", methods=["GET"])
+@admin_profiles_api_bp.route("/", methods=["GET"])
 @admin_api_required
 def get_users(user):
     """Fetch all users via RPC."""
