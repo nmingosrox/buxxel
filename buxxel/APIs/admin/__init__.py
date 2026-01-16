940b05362b601644c58ab2a8bf731f077283a29b
nmingosrox <70023818+nmingosrox@users.noreply.github.com>
2026-01-16 12:26:29 +0200

    Update __init__.py

--- buxxel/APIs/admin/__init__.py
+++ buxxel/APIs/admin/__init__.py
@@ -7,7 +7,7 @@ Provides access to admin-specific API blueprints for listings, orders, and users
 
 from .admin_listings_api import admin_listings_api_bp
 from .admin_orders_api import admin_orders_api_bp
-from .admin_profiles_api import admin_users_api_bp
+from .admin_profiles_api import admin_profiles_api_bp
 
 __all__ = [
     "admin_listings_api_bp",
