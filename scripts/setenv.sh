8d7f6dc4ef6443414f3c76e771732e23311db0cb
nmingosrox <fdomingos116@gmail.com>
2026-01-28 16:53:20 +0200

    -m

---
+++ scripts/setenv.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Local development environment variables
+# RUN THIS SCRIPT TO ENVIRONMENT VARIABLES NEEDED TO RUN THE APP
+export SECRET_KEY="Tneconni1.oibaf#"
+export SB_URL="https://qfkvhfkobovytrqmjhrr.supabase.co"
+export SB_PUBLISHABLE_KEY="sb_publishable_ZloZRbNnxxVC9ptcatnPHw_Ztwmeko5"
+export SB_SECRET_KEY="sb_secret_NzQx4Zmi7_zBqwI_TZ80vw_DPHixmxk"
+export UC_PUBLIC_KEY="82cfba4bb62588a9cda7"
+export UC_SECRET_KEY="dda738f73b4a99c72175"
+
+echo "✅ Local environment variables set for development"
